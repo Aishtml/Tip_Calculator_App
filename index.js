8c2a7cdf878f54113093eb55a47a447b82fc540c
Aisosa Matthew <aisosamatthew247@gmail.com>
2025-07-20 16:58:16 +0100

    created the functionality for if the user uses input field or button for rating and also error handling.

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
+const rates = document.querySelectorAll('.input__rate button');
+let newRate = 0;
 
-const rate = document.querySelector('.input__rate');
-let newRate;
 
-rate.addEventListener('click', (event) => {
-    newRate = +event.target.value;
+rates.forEach(rate => {
+    rate.addEventListener('click', (event) => {
+        newRate = parseInt(event.target.value);
+    });
 });
+
 
 document.querySelector('.tip__btn').addEventListener('click', tipCalculation);
 
 function tipCalculation(event) {
     event.preventDefault();
 
-    const bill = document.querySelector('#amount').value;
-    const numOfPeople = document.querySelector('#people').value;
+    const bill = parseFloat(document.querySelector('#amount').value);
+    const numOfPeople = parseInt(document.querySelector('#people').value);
+    const inputRate = parseFloat(document.querySelector('.input__rate input').value);
+    console.log(inputRate)
 
-    if(!bill || !numOfPeople || !newRate) {
-        alert('FIELD CAN\'T BE EMPTY');
+    if (!bill || !numOfPeople || (isNaN(inputRate) && isNaN(newRate))) {
+    alert('FIELDS CAN\'T BE EMPTY');
+    return;
     }
 
-    let tipPerPerson = (+bill * (+newRate / 100) / +numOfPeople);
-    let totalTip =  +tipPerPerson + (+bill / +numOfPeople);
-    renderHtml(tipPerPerson, totalTip);
+    //use newRate if newRate is not 0 or less, use input if not 0 or less, use 0
+    const rateToUse = newRate > 0 ? newRate : (inputRate > 0 ? inputRate : 0);
+
+    if (rateToUse === 0) {
+        alert('Please select or enter a valid rate.');
+        return; // Stop if there's no valid rate
+    } else if(+bill === 0) {
+        alert('Please select or enter a valid tip amount.');
+        return;
+    } else if(numOfPeople === 0) {
+        alert('Please select or enter a valid number of person.');
+        return;
+    } else {
+        const tipPerPerson = (bill * (rateToUse / 100)) / numOfPeople;
+        const totalTip = tipPerPerson + (bill / numOfPeople);
+        
+        renderHtml(tipPerPerson, totalTip);
+    }
+
+    
+
+    
 }
 
 
